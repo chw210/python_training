833ed5d5bbe37a2aafcf58388b1d09738d65a85a
Chenglong Wang <chenglong.wang@ansys.com>
2017-11-02 15:10:10 -0400

    code_loop

--- 05loops.py
+++ 05loops.py
@@ -7,8 +7,21 @@ def main():
     #   x = x + 1
 
     # define a for loop
-    for x in range(5,10):
-        print x
+    # for x in range(5,10):
+    #    print x
+
+    #days = ["Mon", "Tue"]
+    #for d in days:
+    #    print d
+
+    #for x in range(5,10):
+    #    #if (x == 7): break
+    #    if (x % 2 == 0): continue
+    #    print x
+
+    days = ["Mon", "Tue"]
+    for i, d in enumerate(days):
+        print i, d
 
 if __name__ == "__main__":
     main()
