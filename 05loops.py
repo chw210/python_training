42433dec63070062e5f59ccfa5d0ac40e72e7f1b
Chenglong Wang <chenglong.wang@ansys.com>
2017-11-02 15:04:41 -0400

    code_loop

---
+++ 05loops.py
@@ -0,0 +1,14 @@
+def main():
+    x = 0
+
+    #define a while loop
+    #while(x<5):
+    #   print x
+    #   x = x + 1
+
+    # define a for loop
+    for x in range(5,10):
+        print x
+
+if __name__ == "__main__":
+    main()
